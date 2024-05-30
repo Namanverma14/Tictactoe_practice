9945bbfc12cdadabb3dcf203f4a74a1400cc0e4c
Naman Verma <namanverma1402@gmail.com>
2024-05-30 14:04:25 +0530

    Adding Files

---
+++ script.js
@@ -0,0 +1,62 @@
+console.log("Welcome to Tic Tac Toe")
+let audioTurn = new Audio("button.mp3")
+let gameover = new Audio("won.mp3")
+let turn = "X"
+let game = false
+//Function to change turn
+const changeTurn=()=>{
+    return turn === "X"?"0":"X";
+}
+//Function to check win
+const checkWin=()=>{
+    let boxtext = document.getElementsByClassName("boxtext")
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+    wins.forEach(e=>{
+        if((boxtext[e[0]].innerText !== "")&&(boxtext[e[0]].innerText === boxtext[e[1]].innerText)&&(boxtext[e[0]].innerText === boxtext[e[2]].innerText)){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            game = true
+            document.querySelector('.imagebox').getElementsByTagName('img')[0].style.width = "300px"
+            document.querySelector('.line').style.width = "20vw"
+            document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            gameover.play()
+        }
+    })
+}
+
+//Game Logic
+let boxes = document.getElementsByClassName("box")
+Array.from(boxes).forEach(element=>{
+    let boxtext = element.querySelector('.boxtext')
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn()
+            audioTurn.play()
+            checkWin()
+            if(!game){
+                document.getElementsByClassName("info")[0].innerText = "Turn for "+turn
+            }
+        }
+    })
+})
+//Add onclick listener to reset button
+reset.addEventListener('click',()=>{
+    let boxtext = document.querySelectorAll('.boxtext')
+    Array.from(boxtext).forEach(element=>{
+        element.innerText=""
+    })
+    turn = "X"
+    game = false
+    document.getElementsByClassName("info")[0].innerText = "Turn for "+turn
+    document.querySelector('.imagebox').getElementsByTagName('img')[0].style.width = "0px"
+    document.querySelector('.line').style.width = "0vw"
+})
